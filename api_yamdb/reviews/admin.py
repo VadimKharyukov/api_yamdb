0a48ccf49f0a8b1cf962f2b777549e2a534f4edd
VadimKharyukov <83667640+VadimKharyukov@users.noreply.github.com>
2021-09-11 20:21:51 +0300

    'после_мержа_ветки_категории_2'

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 
-from .models import Category, Title, Genre, GenreTitle
+from .models import Category, Title, Genre, GenreTitle, CustomUser
+
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('pk',
+                    'username',
+                    'last_name',
+                    'first_name',
+                    'role'
+                    )
+    search_fields = ('username',)
+    empty_value_display = '-пусто-'
 
 
 class TitleAdmin(admin.ModelAdmin):
@@ -20,12 +31,14 @@ class GenreAdmin(admin.ModelAdmin):
     search_fields = ('name',)
     empty_value_display = '-пусто-'
 
+
 class GenreTitleAdmin(admin.ModelAdmin):
     list_display = ('pk', 'genre', 'title')
-    search_fields = ('genre','title')
+    search_fields = ('genre', 'title')
     empty_value_display = '-пусто-'
 
 
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Title, TitleAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Genre, GenreAdmin)
